4b0a06b996c8074ee90d5a33e30d149e425e4b31
robotanh <robotanh15@gmail.com>
2023-11-24 19:04:50 +0700

    adv

--- Nucleo_board/code/Core/Src/fsm_pedestrian_light.c
+++ Nucleo_board/code/Core/Src/fsm_pedestrian_light.c
@@ -7,4 +7,16 @@
 
 #include "fsm_pedestrian_light.h"
 
+void fsm_pedestrian_light(){
+	switch (status){
+		case AUTO_RED_GREEN:
+			break;
+		case AUTO_YELLOW_RED:
+			break;
+		case AUTO_GREEN_RED:
+			break;
+		default:
+			break;
+	}
+}
 
