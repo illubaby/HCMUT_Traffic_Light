bb3d3a4fad8aaf3a9e7e7cdaee167f995c36a4ca
robotanh <robotanh15@gmail.com>
2023-11-24 18:55:42 +0700

    den nguoi di bo init

---
+++ Nucleo_board/code/Core/Inc/fsm_pedestrian_light.h
@@ -0,0 +1,15 @@
+/*
+ * fsm_pedestrian_light.h
+ *
+ *  Created on: Nov 24, 2023
+ *      Author: TAnh
+ */
+
+#ifndef INC_FSM_PEDESTRIAN_LIGHT_H_
+#define INC_FSM_PEDESTRIAN_LIGHT_H_
+
+#include "global.h"
+
+
+void fsm_pedestrian_light();
+#endif /* INC_FSM_PEDESTRIAN_LIGHT_H_ */
---
+++ Nucleo_board/code/Core/Src/fsm_pedestrian_light.c
@@ -0,0 +1,10 @@
+/*
+ * fsm_pedestrian_light.c
+ *
+ *  Created on: Nov 24, 2023
+ *      Author: Admin
+ */
+
+#include "fsm_pedestrian_light.h"
+
+
