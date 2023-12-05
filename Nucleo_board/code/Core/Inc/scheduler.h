f20126734c4c964d7742638728ce5cbe75cfc997
illubaby <aironb177@gmail.com>
2023-12-05 21:45:09 +0700

    scheduler

--- Nucleo_board/code/Core/Inc/scheduler.h
+++ Nucleo_board/code/Core/Inc/scheduler.h
@@ -1,37 +1,30 @@
 /*
- * scheduler.h
+ * app_scheduler.h
  *
- *  Created on: Nov 30, 2023
- *      Author: Admin
+ *  Created on: Nov 27, 2019
+ *      Author: VAIO
  */
 
-#ifndef INC_SCHEDULER_H_
-#define INC_SCHEDULER_H_
+#ifndef APP_SCHEDULER_H_
+#define APP_SCHEDULER_H_
 
-#include "global.h"
-#include <stdint.h>
+#include "stdint.h"
 
-#define SCH_MAX_TASKS 6
-#define NO_TASK_ID 0
 
-typedef struct {
-	// Pointer to the task (must be a ’ void ( void ) ’ function )
-	void (*pTask)(void);
-	// Delay (ticks) until the function will (next) be run
-	uint32_t Delay;
-	// Interval (ticks) between subsequent runs.
-	uint32_t Period;
-	// Incremented (by scheduler) when task is due to execute
-	uint8_t RunMe;
-	//This is a hint to solve the question below .
-	uint32_t TaskID;
-} sTask;
+#define SCH_MAX_TASKS 			10
+#define	NO_TASK_ID				0
 
-unsigned char SCH_Add_Task(void(*pFunction)(), unsigned int DELAY, unsigned int PERIOD);
-void SCH_Dispatch_Tasks(void);
-unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX);
-void SCH_Init(void);
-void SCH_Update(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern sTask SCH_tasks_G[SCH_MAX_TASKS];
-#endif /* INC_SCHEDULER_H_ */
+    void SCH_Init(void);
+    void SCH_Update(void);
+    uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
+    void SCH_Dispatch_Tasks(void);
+    uint8_t SCH_Delete_Task(uint32_t TASK_ID);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* APP_SCHEDULER_H_ */
--- Nucleo_board/code/Core/Src/scheduler.c
+++ Nucleo_board/code/Core/Src/scheduler.c
@@ -1,107 +1,158 @@
+/*
+ * app_scheduler.c
+ *
+ *  Created on: Nov 27, 2019
+ *      Author: VAIO
+ */
+//#include "Arduino.h"
+#include "scheduler.h"
+#include "stm32f1xx_hal.h"
 
 
-#include"Scheduler.h"
+typedef struct {
+	void ( * pTask)(void);
+	uint32_t Delay;
+	uint32_t Period;	// task is periodic or not
+	uint8_t RunMe;	// run or not
+	uint32_t TaskID;
+} sTask;
 
-sTask SCH_tasks_G[SCH_MAX_TASKS];
+// The array of tasks
+static sTask SCH_tasks_G[SCH_MAX_TASKS];
 
-unsigned char SCH_Add_Task(void(*pFunction)(), unsigned int DELAY, unsigned int PERIOD) {
-	unsigned char Index = 0;
-	// First find a gap in the array (if there is one)
-	while((SCH_tasks_G[Index].pTask != 0) && (Index < SCH_MAX_TASKS)) {
-		Index++;
-	}
+static uint32_t newTaskID = 0;
 
-	// Have we reached the end of the list?
-	if(Index == SCH_MAX_TASKS) {
-		// Task list is full
-		// Set the global error variable
-		// Error_code_G = ERROR_SCH_TOO_MANY_TASKS;
-		// Also return an error code
-		return SCH_MAX_TASKS;
-	}
-	// If we ’re here, there is a space in the task array
-	SCH_tasks_G[Index].pTask = pFunction;
-	SCH_tasks_G[Index].Delay = DELAY;
-	SCH_tasks_G[Index].Period = PERIOD;
-	SCH_tasks_G[Index].RunMe = 0;
+static uint32_t Get_New_Task_ID(void);
+unsigned char Error_code_G = 0;
 
-	// Return position of task (to allow later deletion)
-	return Index;
+void SCH_Init(void){
+    unsigned char i;
+    for (i = 0; i < SCH_MAX_TASKS; i++) {
+        SCH_Delete_Task(i);
+    }
 }
 
-void SCH_Dispatch_Tasks(void) {
-	unsigned char Index;
-	// Dispatches (runs) the next task (if one is ready)
-	for(Index = 0; Index < SCH_MAX_TASKS; Index++) {
-		if(SCH_tasks_G[Index].RunMe > 0) {
-			(*SCH_tasks_G[Index].pTask)(); // Run the task
-			SCH_tasks_G[Index].RunMe -= 1; // Reset / reduce RunMe flag
-			// Periodic tasks will automatically run again
-			// if this is a ’one shot’ task, remove it from the array
-			// if(SCH_tasks_G[Index].Period == 0) {
-				// SCH_Delete_Task(Index);
-			// }
+void SCH_Update(void){
+	//CHECK ALL TASK 'S DELAY TIME. IF RUN OUT, RUN THAT TASK
+	if (SCH_tasks_G[0].pTask && SCH_tasks_G[0].RunMe == 0) {
+		if(SCH_tasks_G[0].Delay > 0){
+			SCH_tasks_G[0].Delay = SCH_tasks_G[0].Delay - 1;
+		}
+		if (SCH_tasks_G[0].Delay == 0) {
+			SCH_tasks_G[0].RunMe = 1;
 		}
 	}
-	// Report system status
-//	SCH_Report_Status();
-	// The scheduler enters idle mode at this point
-//	SCH_Go_To_Sleep();
 }
+uint32_t SCH_Add_Task(void (* pFunction)(), uint32_t DELAY, uint32_t PERIOD){
+	uint8_t newTaskIndex = 0;
+	uint32_t sumDelay = 0;
+	uint32_t newDelay = 0;
 
-unsigned char SCH_Delete_Task(const unsigned char TASK_INDEX) {
-	unsigned char Return_code = 0;
-	if(SCH_tasks_G[TASK_INDEX].pTask == 0) {
-		// No task at this location ...
-
-		// Set the global error variable
-		// Error_code_G = ERROR_SCH_CANNOT_DELETE_TASK
-
-		// ... also return an error code
-		// Return_code = RETURN_ERROR;
-	} else {
-		// Return_code = RETURN_NORMAL;
-	}
-
-	SCH_tasks_G[TASK_INDEX].pTask = NULL;
-	SCH_tasks_G[TASK_INDEX ].Delay = 0;
-	SCH_tasks_G[TASK_INDEX ].Period = 0;
-	SCH_tasks_G[TASK_INDEX ].RunMe = 0;
-
-	return Return_code;
-}
-
-void SCH_Init(void) {
-	unsigned char i ;
-	for(i = 0; i < SCH_MAX_TASKS; i++) {
-		SCH_Delete_Task(i);
-	}
-	// Reset the global error variable
-	// − SCH_Delete_Task ( ) will generate an e r ro r code ,
-	// ( because the task array i s empty)
-	// Error_code_G = 0;
-	// Timer_init();
-	// Watchdog_init ( ) ;
-}
-
-void SCH_Update(void) {
-	unsigned char Index;
-	// NOTE: calculations are in *TICKS* (not milliseconds)
-	for(Index = 0; Index < SCH_MAX_TASKS; Index++) {
-		// Check if there is a task at this location
-		if(SCH_tasks_G[Index].pTask) {
-			if(SCH_tasks_G[Index].Delay == 0) {
-				// The task is due to run
-				// Inc . the ’RunMe’ flag
-				SCH_tasks_G[Index].RunMe += 1;
-				if(SCH_tasks_G[Index].Period) {
-					// Schedule periodic tasks to run again
-					SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
+	for(newTaskIndex = 0; newTaskIndex < SCH_MAX_TASKS; newTaskIndex ++){
+		sumDelay = sumDelay + SCH_tasks_G[newTaskIndex].Delay;
+		if(sumDelay > DELAY){
+			//ADD NEW TASK BEFORE THE CURRENT TASK
+			newDelay = DELAY - (sumDelay - SCH_tasks_G[newTaskIndex].Delay);
+			SCH_tasks_G[newTaskIndex].Delay = sumDelay - DELAY;
+				// shift the tasks in the Array
+			for(uint8_t i = SCH_MAX_TASKS - 1; i > newTaskIndex; i --){
+//				if(SCH_tasks_G[i - 1].pTask != 0)
+				{
+					SCH_tasks_G[i].pTask = SCH_tasks_G[i - 1].pTask;
+					SCH_tasks_G[i].Period = SCH_tasks_G[i - 1].Period;
+					SCH_tasks_G[i].Delay = SCH_tasks_G[i - 1].Delay;
+					SCH_tasks_G[i].RunMe = SCH_tasks_G[i - 1].RunMe;
+					SCH_tasks_G[i].TaskID = SCH_tasks_G[i - 1].TaskID;
 				}
+			}
+			// insert the new task
+			SCH_tasks_G[newTaskIndex].pTask = pFunction;
+			SCH_tasks_G[newTaskIndex].Delay = newDelay;
+			SCH_tasks_G[newTaskIndex].Period = PERIOD;
+			if(SCH_tasks_G[newTaskIndex].Delay == 0){
+				SCH_tasks_G[newTaskIndex].RunMe = 1;
 			} else {
-				// Not yet ready to run: just decrement the delay
-				SCH_tasks_G[Index].Delay -= 1;
+				SCH_tasks_G[newTaskIndex].RunMe = 0;
+			}
+			SCH_tasks_G[newTaskIndex].TaskID = Get_New_Task_ID();
+			return SCH_tasks_G[newTaskIndex].TaskID;
+		} else {
+			// One an empty slot is found, the new task is added there with the remaning delay
+			if(SCH_tasks_G[newTaskIndex].pTask == 0x0000){
+				SCH_tasks_G[newTaskIndex].pTask = pFunction;
+				SCH_tasks_G[newTaskIndex].Delay = DELAY - sumDelay;
+				SCH_tasks_G[newTaskIndex].Period = PERIOD;
+				if(SCH_tasks_G[newTaskIndex].Delay == 0){
+					SCH_tasks_G[newTaskIndex].RunMe = 1;
+				} else {
+					SCH_tasks_G[newTaskIndex].RunMe = 0;
+				}
+				SCH_tasks_G[newTaskIndex].TaskID = Get_New_Task_ID();
+				return SCH_tasks_G[newTaskIndex].TaskID;
 			}
 		}
 	}
+	return SCH_tasks_G[newTaskIndex].TaskID;
 }
+
+
+uint8_t SCH_Delete_Task(uint32_t taskID){
+	uint8_t Return_code  = 0;
+	uint8_t taskIndex;
+	uint8_t j;
+	if(taskID != NO_TASK_ID){
+		for(taskIndex = 0; taskIndex < SCH_MAX_TASKS; taskIndex ++){
+			// Searching task for delete
+			if(SCH_tasks_G[taskIndex].TaskID == taskID){
+				Return_code = 1;
+				if(taskIndex != 0 && taskIndex < SCH_MAX_TASKS - 1){
+					if(SCH_tasks_G[taskIndex+1].pTask != 0x0000){
+						SCH_tasks_G[taskIndex+1].Delay += SCH_tasks_G[taskIndex].Delay;
+					}
+				}
+				// after delete the task, shift the array
+				for( j = taskIndex; j < SCH_MAX_TASKS - 1; j ++){
+					SCH_tasks_G[j].pTask = SCH_tasks_G[j+1].pTask;
+					SCH_tasks_G[j].Period = SCH_tasks_G[j+1].Period;
+					SCH_tasks_G[j].Delay = SCH_tasks_G[j+1].Delay;
+					SCH_tasks_G[j].RunMe = SCH_tasks_G[j+1].RunMe;
+					SCH_tasks_G[j].TaskID = SCH_tasks_G[j+1].TaskID;
+				}
+				SCH_tasks_G[j].pTask = 0;
+				SCH_tasks_G[j].Period = 0;
+				SCH_tasks_G[j].Delay = 0;
+				SCH_tasks_G[j].RunMe = 0;
+				SCH_tasks_G[j].TaskID = 0;
+				return Return_code;
+			}
+		}
+	}
+	return Return_code; // return status
+}
+
+void SCH_Dispatch_Tasks(void){
+	// CHECK IF THE TASK READY TO RUN. AFTER FINISHED RUNNING, DELETE THAT TASK AND ADD IT AGAIN
+	if(SCH_tasks_G[0].RunMe > 0) {
+		(*SCH_tasks_G[0].pTask)();
+		SCH_tasks_G[0].RunMe = 0;
+		sTask temtask = SCH_tasks_G[0];
+		SCH_Delete_Task(temtask.TaskID);
+		if (temtask.Period != 0) {
+			SCH_Add_Task(temtask.pTask, temtask.Period, temtask.Period);
+		}
+	}
+	// Enter low-power mode (Sleep mode). The MCU will wake up on the next interrupt
+	    HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+}
+
+static uint32_t Get_New_Task_ID(void){
+	newTaskID++;
+	if(newTaskID == NO_TASK_ID){
+		newTaskID++;
+	}
+	return newTaskID;
+}
+
+#ifdef __cplusplus
+}
+#endif
