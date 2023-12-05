57fb21d2f72ddab37916a26ed46e81cb0fc420fb
illubaby <aironb177@gmail.com>
2023-12-05 21:31:02 +0700

    Update buzzer.c

--- Nucleo_board/code/Core/Src/buzzer.c
+++ Nucleo_board/code/Core/Src/buzzer.c
@@ -10,20 +10,21 @@
 #include "main.h"
 extern TIM_HandleTypeDef htim3;  // External declaration
 int buzzer_counter;
-int state_buzzer = 1	;
-int scale_buzzer=0;
+int state_buzzer = 1;
+float scale_buzzer=0;
 float cycle;
 void get_buzzer_counter(){	// for take the value of the red time
 	buzzer_counter = red_time/2;
 }
-void cal_cycle (){
-	//cycle = ((float)buzzer_counter / red_time) * 100.0f;	// the speaker decreases volume every 1s
-	cycle = ((float)buzzer_counter / red_time) * 100.0f-scale_buzzer; // the speaker decreases volume every 100ms
-	scale_buzzer++;
-	if (scale_buzzer>=10){
-		scale_buzzer=0;
-	}
+void cal_cycle() {
+    //cycle = ((float)buzzer_counter / red_time) * 100.0f;	// the speaker decreases volume every 1s
+    cycle = ((float)buzzer_counter / red_time) * 100.0f - scale_buzzer;// the speaker decreases volume every 100ms
+    scale_buzzer += (float) (1 / (red_time))*100/10;
+    if (scale_buzzer >= (1 / (red_time))*100) {
+        scale_buzzer = 0;
+    }
 }
+
 void buzzer(){
 	switch(state_buzzer){
 	case ON:
